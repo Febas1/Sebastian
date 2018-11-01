610b7f0b8a93124666db2f03368223bbab850780
Juan Sebastian Solano Casallas <jssolano@unipanamericana.edu.co>
2018-11-01 17:04:22 -0500

    Modificacion en doc colores

---
+++ R506/R506.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Win32;
+using Syncfusion.UI.Xaml.Grid.Converter;
+using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace SiasoftAppExt
+{
+    
+    public partial class R506 : UserControl
+    {
+
+        dynamic SiaWin;
+        dynamic tabitem;
+        int idemp = 0;
+        string cnEmp = "";
+
+
+
+        public R506(dynamic tabitem1)
+        {
+            InitializeComponent();
+            SiaWin = Application.Current.MainWindow;
+            tabitem = tabitem1;
+            idemp = SiaWin._BusinessId;
+
+
+            LoadConfig();
+        }
+
+
+        private void LoadConfig()
+        {
+            try
+            {
+                System.Data.DataRow foundRow = SiaWin.Empresas.Rows.Find(idemp);
+                int idLogo = Convert.ToInt32(foundRow["BusinessLogo"].ToString().Trim());
+                idemp = Convert.ToInt32(foundRow["BusinessId"].ToString().Trim());
+                cnEmp = foundRow[SiaWin.CmpBusinessCn].ToString().Trim();
+                string aliasemp = foundRow["BusinessAlias"].ToString().Trim();
+                tabitem.Logo(idLogo, ".png");
+                tabitem.Title = "R506(" + aliasemp + ")";
+
+                FecIni.Text = DateTime.Now.ToShortDateString();
+                FecFin.Text = DateTime.Now.ToShortDateString();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                SqlConnection con = new SqlConnection(cnEmp);
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                cmd = new SqlCommand("Informe_R506", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FechaIni", FecIni.Text.ToString());
+                cmd.Parameters.AddWithValue("@FechaFin", FecFin.Text.ToString());                
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+                GridKardex.ItemsSource = ds.Tables[0];
+                }
+            catch (Exception w)
+            {
+                MessageBox.Show("error al cargar la consulta programada" + w);
+            }
+
+        }
+
+        private static void CellExportingHandler(object sender, GridCellExcelExportingEventArgs e)
+        {
+
+            
+            if (e.ColumnName == "cod_ref")
+            {
+                e.Range.CellStyle.NumberFormat = "0";
+                //e.Range.CellStyle.Font.Color = ExcelKnownColors.Blue;
+                //e.Range.CellStyle.Font.FontName = "Segoe UI";
+
+
+            }
+          
+        }
+
+        private void ExportaXLS_Click(object sender, RoutedEventArgs e)
+        {
+            var options = new Syncfusion.UI.Xaml.Grid.Converter.ExcelExportingOptions();
+            options.ExcelVersion = ExcelVersion.Excel2013;
+            options.CellsExportingEventHandler = CellExportingHandler;
+
+            var excelEngine = GridKardex.ExportToExcel(GridKardex.View, options);
+            var workBook = excelEngine.Excel.Workbooks[0];
+
+
+
+
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                FilterIndex = 2,
+                Filter = "Excel 97 to 2003 Files(*.xls)|*.xls|Excel 2007 to 2010 Files(*.xlsx)|*.xlsx|Excel 2013 File(*.xlsx)|*.xlsx"
+            };
+
+
+
+            if (sfd.ShowDialog() == true)
+            {
+                using (Stream stream = sfd.OpenFile())
+                {
+                    if (sfd.FilterIndex == 1)
+                        workBook.Version = ExcelVersion.Excel97to2003;
+                    else if (sfd.FilterIndex == 2)
+                        workBook.Version = ExcelVersion.Excel2010;
+                    else
+                        workBook.Version = ExcelVersion.Excel2013;
+                    workBook.SaveAs(stream);
+                }
+
+                //Message box confirmation to view the created workbook.
+                if (MessageBox.Show("Usted quiere abrir el archivo en excel?", "Ver archvo",
+                                    MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+                    //Launching the Excel file using the default Application.[MS Excel Or Free ExcelViewer]
+                    System.Diagnostics.Process.Start(sfd.FileName);
+                }
+            }
+
+
+        }
+
+
+
+
+    }
+}
--- Sebastian/Colores.xaml.cs
+++ Sebastian/Colores.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +18,107 @@ using System.Windows.Shapes;
 
 namespace Sebastian
 {
-    
+
     public partial class Colores : UserControl
     {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+        #region IDataErrorInfo Members
+        public string Error
+        {
+            get { return this[String.Empty]; }
+        }
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = String.Empty;
+                if (columnName == "Name")
+                {
+                    if (string.IsNullOrEmpty(this.Name))
+                    {
+                        result = "Name is mandatory";
+                    }
+                }
+                if (columnName == "Age")
+                {
+                    if (this.Age < 1 || this.Age > 100)
+                    {
+                        result = "Invalid Age";
+                    }
+                }
+                return result;
+            }
+        }
+        #endregion
+        public class ViewModel
+        {
+            public ObservableCollection<Colores> Coloress { get; set; }
+            public ViewModel()
+            {
+                Coloress = new ObservableCollection<Colores>();
+                PopulateData();
+            }
+            private void PopulateData()
+            {
+                Coloress.Add(new Colores() { Name = "John", Age = 26 });
+                Coloress.Add(new Colores() { Name = "Mark", Age = 25 });
+                Coloress.Add(new Colores() { Name = "Steven", Age = 26 });
+            }
+        }
+        dynamic SiaWin;
+        dynamic tabitem;
+        int idemp = 0;
+        string cnEmp = "";
         public Colores()
         {
             InitializeComponent();
+        }
+        private DataSet LoadData()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(cnEmp);
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                cmd = new SqlCommand("UxPaleta", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+                foreach (DataTable table in ds.Tables)
+                {
+                    //            newColumn.DefaultValue = "Your DropDownList value";
+                    System.Data.DataColumn newColumn = new System.Data.DataColumn("ven_net", typeof(System.Double));
+                    System.Data.DataColumn newColumn1 = new System.Data.DataColumn("util", typeof(System.Double));
+                    System.Data.DataColumn newColumn2 = new System.Data.DataColumn("por_util", typeof(System.Double));
+                    System.Data.DataColumn newColumn3 = new System.Data.DataColumn("por_parti", typeof(System.Double));
+                    System.Data.DataColumn newColumn4 = new System.Data.DataColumn("can_net", typeof(System.Double));
+                    ds.Tables[table.TableName].Columns.Add(newColumn);
+                    ds.Tables[table.TableName].Columns.Add(newColumn1);
+                    ds.Tables[table.TableName].Columns.Add(newColumn2);
+                    ds.Tables[table.TableName].Columns.Add(newColumn3);
+                    ds.Tables[table.TableName].Columns.Add(newColumn4);
+                }
+                return ds;
+                //VentasPorProducto.ItemsSource = ds.Tables[0];
+                //VentaPorBodega.ItemsSource = ds.Tables[1];
+                //VentasPorCliente.ItemsSource = ds.Tables[2];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
         }
 
 
